28c44c8fbf25e73c786fd01646632be2813f7173
zh99998 <zh99998@gmail.com>
2013-01-21 11:22:06 +0800

    links

--- assets/javascripts/mycard.coffee
+++ assets/javascripts/mycard.coffee
@@ -5,9 +5,22 @@ $(document).ready ->
     fx:'fade'
     timeout:7200
     random:1
-
+	
+  #version
   $.get '/mycard/download.url', (data)->
     if matched = data.match(/mycard-(.*)-win32\.7z/)
       $('#download_version').html matched[1]
     else
       $('#download_version').html '读取失败'
+	  
+  #link
+  $.getJSON '/links.json', (data)->
+    for link in data
+      $('<a />', 
+        href: link.url
+      ).append($('<img />',
+        title: link.name
+        alt: link.name
+        src: link.logo
+      )).appendTo('#links')
+  
--- assets/javascripts/mycard.js
+++ assets/javascripts/mycard.js
@@ -7,13 +7,28 @@
       timeout: 7200,
       random: 1
     });
-    return $.get('/mycard/download.url', function(data) {
+    $.get('/mycard/download.url', function(data) {
       var matched;
       if (matched = data.match(/mycard-(.*)-win32\.7z/)) {
         return $('#download_version').html(matched[1]);
       } else {
         return $('#download_version').html('读取失败');
       }
+    });
+    return $.getJSON('/links.json', function(data) {
+      var link, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = data.length; _i < _len; _i++) {
+        link = data[_i];
+        _results.push($('<a />', {
+          href: link.url
+        }).append($('<img />', {
+          title: link.name,
+          alt: link.name,
+          src: link.logo
+        })).appendTo('#links'));
+      }
+      return _results;
     });
   });
 
