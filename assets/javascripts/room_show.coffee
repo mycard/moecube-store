94c38fe1cde63a180836ab9e29d1e51a08c0ccad
zh99998 <zh99998@gmail.com>
2013-02-08 11:18:38 +0800

    login

--- assets/javascripts/room_show.coffee
+++ assets/javascripts/room_show.coffee
@@ -1,3 +1,20 @@
+login = (username, password)->
+  $('#username').html username
+  $('#login').hide()
+  $('#userinfo').show()
+  $('#need_login').hide()
+  $('#join').show()
+
+$('#logout').click ->
+  $.cookie('username', '')
+  $.cookie('password', '')
+  window.location.reload()
+$('#login').submit ->
+  $.cookie('username', @username.value)
+  $.cookie('password', @password.value)
+  login($.cookie('username'), $.cookie('password'))
+  false
+
 matched = window.location.href.match /\/(?:(.*?)(?::(.*?))?@)?([\d\.]+)\:(\d+)(?:\/(.*))?/
 if matched == null
   alert "解析房间信息失败"
@@ -31,5 +48,12 @@ $('#server_ip').html room.server.ip
 $('#server_port').html room.server.port
 $('#server_auth').html room.server.auth
 
+if room.server.auth and !($.cookie('username') && $.cookie('password'))
+  $('#join').hide()
+  $('#need_login').show()
+
+if $.cookie('username') && $.cookie('password')
+  login($.cookie('username'), $.cookie('password'))
+
 $('#join').click ->
-  mycard.join room.server.ip,room.server.port,mycard.room_name(room.name, room.password)
+  mycard.join room.server.ip,room.server.port,mycard.room_name(room.name, room.password), $.cookie('username'), ($.cookie('password') if room.server.auth)
--- vendor/javascripts/jquery.cookie.js
+++ vendor/javascripts/jquery.cookie.js
@@ -1,11 +1,19 @@
 /*!
- * jQuery Cookie Plugin v1.3.0
+ * jQuery Cookie Plugin v1.3.1
  * https://github.com/carhartl/jquery-cookie
  *
  * Copyright 2013 Klaus Hartl
  * Released under the MIT license
  */
-(function ($, document, undefined) {
+(function (factory) {
+	if (typeof define === 'function' && define.amd && define.amd.jQuery) {
+		// AMD. Register as anonymous module.
+		define(['jquery'], factory);
+	} else {
+		// Browser globals.
+		factory(jQuery);
+	}
+}(function ($) {
 
 	var pluses = /\+/g;
 
@@ -14,15 +22,17 @@
 	}
 
 	function decoded(s) {
-		return unRfc2068(decodeURIComponent(s.replace(pluses, ' ')));
+		return decodeURIComponent(s.replace(pluses, ' '));
 	}
-	
-	function unRfc2068(value) {
-		if (value.indexOf('"') === 0) {
+
+	function converted(s) {
+		if (s.indexOf('"') === 0) {
 			// This is a quoted cookie as according to RFC2068, unescape
-			value = value.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
 		}
-		return value;
+		try {
+			return config.json ? JSON.parse(s) : s;
+		} catch(er) {}
 	}
 
 	var config = $.cookie = function (key, value, options) {
@@ -30,10 +40,6 @@
 		// write
 		if (value !== undefined) {
 			options = $.extend({}, config.defaults, options);
-
-			if (value === null) {
-				options.expires = -1;
-			}
 
 			if (typeof options.expires === 'number') {
 				var days = options.expires, t = options.expires = new Date();
@@ -54,23 +60,19 @@
 		// read
 		var decode = config.raw ? raw : decoded;
 		var cookies = document.cookie.split('; ');
-		var result = key ? null : {};
+		var result = key ? undefined : {};
 		for (var i = 0, l = cookies.length; i < l; i++) {
 			var parts = cookies[i].split('=');
 			var name = decode(parts.shift());
 			var cookie = decode(parts.join('='));
-			
-			if (config.json) {
-				cookie = JSON.parse(cookie);
-			}
-			
+
 			if (key && key === name) {
-				result = cookie;
+				result = converted(cookie);
 				break;
 			}
-			
+
 			if (!key) {
-				result[name] = cookie;
+				result[name] = converted(cookie);
 			}
 		}
 
@@ -80,11 +82,11 @@
 	config.defaults = {};
 
 	$.removeCookie = function (key, options) {
-		if ($.cookie(key) !== null) {
-			$.cookie(key, null, options);
+		if ($.cookie(key) !== undefined) {
+			$.cookie(key, '', $.extend(options, { expires: -1 }));
 			return true;
 		}
 		return false;
 	};
 
-})(jQuery, document);
+}));
