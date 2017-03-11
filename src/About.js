fb172b85105dc58f737bd83484ebb07164db14cc
HJISTC <hjistc@moecube.com>
2017-03-11 14:29:17 +0800

    Warning free

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ import { Layout } from 'antd'
 
 import Nav from './Nav'
 
-const { Content, Footer, Header } = Layout
+const { Header } = Layout
 
 export default class About extends React.Component {
     constructor(props) {
@@ -14,8 +14,8 @@ export default class About extends React.Component {
 
         this.state = {
             isMobile: false,
-            stats: { signups: null, online: null },
-            latest: { win32: {}, drawin: {} },
+            //stats: { signups: null, online: null },
+            //latest: { win32: {}, drawin: {} },
             platform: navigator.platform.match(/Mac/i) ? 'drawin' : 'win32'
         }
     }
@@ -30,25 +30,26 @@ export default class About extends React.Component {
             }
         })
 
-        const initState = {
-            stats: {
-                signups: await this.get_stats_signups(),
-                online: await this.get_stats_online()
-            }
-        }
-        const initLatest = {
-            latest: {
-                win32: await this.get_latest_win32(),
-                drawin: await this.get_latest_drawin()
-            }
-        }
+        // const initState = {
+        //     stats: {
+        //         signups: await this.get_stats_signups(),
+        //         online: await this.get_stats_online()
+        //     }
+        // }
 
-        this.setState(initState)
-        this.setState(initLatest)
+        // const initLatest = {
+        //     latest: {
+        //         win32: await this.get_latest_win32(),
+        //         drawin: await this.get_latest_drawin()
+        //     }
+        // }
+
+        // this.setState(initState)
+        // this.setState(initLatest)
     }
 
     render() {
-        const { latest, isMobile, stats } = this.state
+        const { isMobile } = this.state // const { latest, isMobile, stats } = this.state
         return (
             <Layout>
                 {!isMobile ?
